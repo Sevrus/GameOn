2241c48aabb3eb20adc8e672087972bdcda50d58
Sevrus <book-n-troll@nordnet.fr>
2024-04-25 17:34:59 +0200

    Refactored CSS to remove duplicates, integrated Google Fonts for typography consistency. Renamed variables in JavaScript and updated class names in HTML for clarity. Also provided more precise comments for functions in JavaScript.

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,23 +1,24 @@
-function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-
 // DOM Elements
-const modalbg = document.querySelector(".bground");
+const modalBg = document.querySelector(".background-modal");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+const formData = document.querySelectorAll(".form-data");
 
-// launch modal event
+// Event Listeners
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
+/**
+ * Toggles the responsive class on the top navigation element.
+ * If the top navigation element has the class "top-nav", adds the class "responsive";
+ * otherwise, sets the class to "top-nav".
+ */
+function editNav() {
+  const topNavElement = document.getElementById("top-nav");
+  topNavElement.className === "top-nav" ? topNavElement.className += " responsive" : topNavElement.className = "top-nav";
 }
 
-
+/**
+ * Displays the modal background element.
+ */
+function launchModal() {
+  modalBg.style.display = "block";
+}
